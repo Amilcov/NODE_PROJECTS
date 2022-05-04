52b581cfd76c0947802ff6a6fab1a62beec73ce7
Amilcov <Amilcov@yahoo.com>
2022-05-05 02:40:50 +0300

    Add files via upload

---
+++ Week09/Day1/Import_Export_ES6_module/Window Object Debugging Project/assets/js/index.js
@@ -0,0 +1,15 @@
+// Imports 
+
+import { changeHeader, changeTitle, changeAboutMe } from "./manipulate.js";
+import { findElementById, findFirstElementOfTag, findFirstElementOfClass, findElementsOfTag, findElementsOfClass} from "./search.js";
+
+window.onload = () => {
+    changeTitle();
+    changeHeader();
+    changeAboutMe();
+    console.log(findElementById("header"));
+    console.log(findFirstElementOfTag("LI"));
+    console.log(findFirstElementOfClass("section"));
+    console.log(findElementsOfTag("H3"));
+    console.log(findElementsOfClass("section"));
+}
---
+++ Week09/Day1/Import_Export_ES6_module/Window Object Debugging Project/assets/js/search.js
@@ -0,0 +1,28 @@
+export function findElementById(id) {
+    // Return the element in the DOM with corresponding `id`
+    return document.getElementById(id);
+}
+
+export  function findFirstElementOfTag(tag) {
+    // Return the first occurence of an element of tag name `tag`
+    return document.querySelectorAll(tag)[0];
+}
+
+export function findFirstElementOfClass(cls) {
+    // Return the first occurence of an element of class `cls`
+    return document.getElementsByClassName(cls)[0];
+    
+}
+
+export function findElementsOfTag(tag) {
+    // Return an array of elements that have a tag name of `tag`
+    return document.querySelectorAll(tag);
+    
+}
+
+export function findElementsOfClass(cls) {
+    // Return an array of elements that have are of class `cls`
+    return document.getElementsByClassName(cls);
+}
+
+
