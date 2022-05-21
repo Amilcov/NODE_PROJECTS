ddefa7ccc9ff6bcac9b1fdb85685faf4be45a797
Amilcov <Amilcov@yahoo.com>
2022-05-21 14:39:36 +0300

    Add files via upload

---
+++ Week09/Day4/RE-DO/1.3 WebStorage_sessionStorage_and_localStorage_Practice/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const index = express();
+index.use(express.static("public"));
+
+const port = 8081;
+
+index.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+index.listen(port, () => console.log(`Listening on port ${port}!`));
+ 
